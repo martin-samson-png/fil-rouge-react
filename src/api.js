0e516a8e9b981a0d6d3b79c0a87d3f04951f94de
martin-samson-png <martinsams64@gmail.com>
2025-08-27 10:41:42 +0200

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: `http://localhost:3001`,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
